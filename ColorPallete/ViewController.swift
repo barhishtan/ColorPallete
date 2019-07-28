de132917462afbcf378c062ef3b5f757312a124b
Artur Sokolov <barhishtan@gmail.com>
2019-07-28 23:20:12 +0200

    3.Добавил зависимости между значениями элементов интерфейса. Установил начальные значения.

--- ColorPallete/ViewController.swift
+++ ColorPallete/ViewController.swift
@@ -12,6 +12,40 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var paletteView: UIView!
     
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redTF: UITextField!
+    @IBOutlet weak var greenTF: UITextField!
+    @IBOutlet weak var blueTF: UITextField!
+    
+    private var redValue: Float! {
+        willSet (newValue) {
+            redLabel.text = String(newValue)
+            redSlider.value = newValue
+            redTF.text = String(newValue)
+        }
+    }
+    private var greenValue: Float! {
+        willSet (newValue) {
+            greenLabel.text = String(newValue)
+            greenSlider.value = newValue
+            greenTF.text = String(newValue)
+        }
+    }
+    private var blueValue: Float!{
+        willячSet (newValue) {
+            blueLabel.text = String(newValue)
+            blueSlider.value = newValue
+            blueTF.text = String(newValue)
+        }
+    }
+    
     override func viewWillLayoutSubviews() {
         paletteView.layer.cornerRadius = paletteView.frame.width * 0.05
         paletteView.layer.borderWidth = 1.0
@@ -20,6 +54,18 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setDefaultValues()
+    }
+    
+    func roundValue(_ value: Float) -> Float {
+        return roundf(value / 0.01) * 0.01
+    }
+    
+    func setDefaultValues() {
+        redValue = roundValue(redSlider.value)
+        greenValue = roundValue(greenSlider.value)
+        blueValue = roundValue(blueSlider.value)
+        paletteView.backgroundColor = UIColor(red: CGFloat(redValue), green: CGFloat(greenValue), blue: CGFloat(blueValue), alpha: 1.0)
     }
 }
 

9be69a37be5ba2e147a67abaff9d0e265ffd29fa
Artur Sokolov <barhishtan@gmail.com>
2019-07-28 20:53:45 +0200

    Initial Commit

---
+++ ColorPallete/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorPallete
+//
+//  Created by Artur Sokolov on 28/07/2019.
+//  Copyright © 2019 Artur Sokolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
